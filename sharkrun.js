7b15368b2b5ea5c1c2f78630263ac0e6066e3527
Lain Nerd <lain@cyberpunk.me>
2018-02-17 14:08:38 +0000

    remove sw

--- sharkrun.js
+++ sharkrun.js
@@ -396,14 +396,4 @@ loadSprites({
   'iceSymbol': 'crystals/symbol.png'
 }, () => {
   startGame()
-})
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+});
